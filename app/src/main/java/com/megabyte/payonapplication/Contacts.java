af7968a6f155c5b05467bbc5138eb271d5caf232
Yousef Mohammed <josephmohammed180@gmail.com>
2025-12-18 23:00:12 +0200

    application can make alot of transaction

--- app/src/main/java/com/megabyte/payonapplication/Contacts.java
+++ app/src/main/java/com/megabyte/payonapplication/Contacts.java
@@ -1,5 +1,6 @@
 package com.megabyte.payonapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,13 +9,37 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Contacts extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class Contacts extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    Intent intent = getIntent();
+    String currentSource = intent.getStringExtra("source");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_contacts);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.navigation_contacts);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.navigation_contacts) {
+                return true;
+            } else if (itemId == R.id.navigation_home) {
+                Intent intent = new Intent(Contacts.this, MainActivity.class);
+                intent.putExtra("source", currentSource);
+                startActivity(intent);
+                return true;
+            } else if (itemId == R.id.navigation_account) {
+                Intent intent = new Intent(Contacts.this, Account.class);
+                intent.putExtra("source", currentSource);
+                startActivity(intent);
+                return true;
+            }
+            return false;
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
